871239bd7818c04f531681fc6c67cdc6f1955d6c
lxl66566 <lxl66566@gmail.com>
2026-09-06 22:55:04 +0800

    perf: drop the rand dependency

--- src/utils.rs
+++ src/utils.rs
@@ -1,5 +1,9 @@
-use rand::distr::{Alphanumeric, SampleString};
+use windows::core::GUID;
 
 pub fn rand_string() -> String {
-    Alphanumeric.sample_string(&mut rand::rng(), 10)
+    // A random (v4) GUID is as good a source of uniqueness as anything, and
+    // avoids pulling in the `rand` crate just for a 10-char string
+    GUID::new()
+        .map(|guid| format!("{guid:?}"))
+        .unwrap_or_default()
 }
